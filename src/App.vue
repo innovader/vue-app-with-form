0fff267b4aef3bc83af7480167c4b33401efd137
innovader <jon_io@hotmail.com>
2022-11-03 15:43:22 +0200

    add-pseudo-navigation-desktop-and-mobile

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
-  <!-- <NavigationMenu /> -->
-  <ContactForm />
+  <NavigationMenu :showMobile="showMobile"/>
+  <!-- <ContactForm /> -->
 </template>
 
 <script>
-// import NavigationMenu from './components/NavigationMenu.vue'
-import ContactForm from './components/ContactForm.vue'
+import NavigationMenu from './components/NavigationMenu.vue'
+//import ContactForm from './components/ContactForm.vue'
 
 export default {
   name: 'App',
   components: {
-    // NavigationMenu,
-    ContactForm
+    NavigationMenu,
+    ///ContactForm
+  },
+  data() {
+      return {
+          windowWidth: window.innerWidth
+      }
+  },
+  mounted() {
+      window.onresize = () => {
+          this.windowWidth = window.innerWidth
+      }
+  },
+  computed: {
+    showMobile() {
+      return this.windowWidth < 1200
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Montserrat', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
--- src/components/NavigationMenu.vue
+++ src/components/NavigationMenu.vue
@@ -1,6 +1,48 @@
 <template>
-  <div class="hello">
-    NavigationMenu component
+  <!-- Desktop Header -->
+  <div v-if="!showMobile" class="header-container desktop">
+    <img class="desktop-logo" src="../assets/Group-1198.svg">
+    <ul class="navigation">
+      <li
+          v-for="(item, index) in items"
+          :key="index"
+          v-text="item.title"
+          class="navigation-item"
+        >
+      </li>
+    </ul>
+  </div>
+
+  <!-- Mobile header -->
+  <div v-if="showMobile && !isMobileMenuShowing" class="header-container">
+
+    <img class="mobile-logo" src="../assets/Group-1198.svg">
+
+    <img
+      class="menu-icon"
+      src="../assets/Group-1154.svg"
+      @click="toggleMobileMenu"
+    >
+  </div>
+
+  <!-- Mobile menu -->
+  <div v-if="showMobile && isMobileMenuShowing" class="mobile-menu">
+    <div class="mobile-menu-title">
+      <span>MENU</span>
+      <img
+        class="close-menu-icon"
+        src="../assets/Group-1156.svg"
+        @click="toggleMobileMenu"
+      >
+    </div>
+    <ul>
+      <li
+          v-for="(item, index) in items"
+          :key="index"
+          v-text="item.title"
+        >
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -8,10 +50,92 @@
 export default {
   name: 'NavigationMenu',
   props: {
+    showMobile: Boolean,
+  },
+  data() {
+    return {
+      isMobileMenuShowing: true,
+      items: [
+        { title: 'Main Page' },
+        { title: 'Page Two' },
+        { title: 'Page Three' },
+        { title: 'About Us' },
+        { title: 'Our Work' },
+        { title: 'Contact' },
+      ],
+    }
+  },
+  methods: {
+    toggleMobileMenu() {
+      this.isMobileMenuShowing = !this.isMobileMenuShowing
+    },
   }
 }
 </script>
 
 <style scoped>
+.header-container {
+  background-color: #000000;
+  color: #FFFFFF;
+  height: 56px;
+  padding: 0px 25px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.desktop-logo {
+  height: 72px;
+  position: absolute;
+  top: 20px;
+  left: 47px;
+}
+
+.desktop {
+  justify-content: center;
+}
+
+.navigation {
+  color: #FFFFFF;
+  font-weight: 800;
+  font-size: 18px;
+  display: flex;
+}
+
+.navigation-item {
+  margin-right: 50px;
+}
+
+.mobile-logo {
+  height: 40px;
+}
+
+.mobile-menu {
+  font-weight: 800;
+  font-size: 38px;
+  line-height: 68px;
+}
+
+.mobile-menu-title {
+  background-color: #000000;
+  color: #FFFFFF;
+  height: 56px;
+  padding: 0px 25px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.close-menu-icon {
+  position: absolute;
+  right: 0;
+  padding-right: 25px;
+}
+
+ul {
+  list-style: none;
+  padding-left: 0;
+  color: #000000;
+}
 
 </style>
